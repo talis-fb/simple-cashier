8876e1a67b4cac26e70f49352220e84fccece169
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2023-07-29 21:59:26 -0300

    fix(frontend): Host to PROD

--- frontend/src/composables/useFetch.ts
+++ frontend/src/composables/useFetch.ts
@@ -1,6 +1,6 @@
 import { useFetch as useFetchVueUse, type UseFetchReturn } from '@vueuse/core'
 
 export const useFetch = <T>(route: string): UseFetchReturn<T> => {
-    const host = 'http://localhost:3000' // process.env.API_HOST || ''
+    const host = '' // process.env.API_HOST || ''
     return useFetchVueUse(host + route)
 }
